adfe81303a94e129bbbd900e94c1938cc61c36cd
JSC <gbs04087@gmail.com>
2022-10-10 21:34:48 +0900

    FEAT: 최적화된 박스 이동

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Test1 from "./pages/Test1";
-import AirPlain2 from "./pages/AirPlane2";
+import BoxMove2 from "./pages/BoxMove2";
 
 function App() {
   return (
     <div className="App">
       {/* <Test1></Test1> */}
-      <AirPlain2></AirPlain2>
+      <BoxMove2></BoxMove2>
     </div>
   );
 }
---
+++ src/pages/BoxMove2.tsx
@@ -0,0 +1,122 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useEffect, useState } from "react";
+
+const Box = () => {
+  const myMesh = React.useRef<any>();
+
+  const [keyPress, setKeyPress] = useState({
+    ArrowLeft: false,
+    ArrowRight: false,
+    ArrowUp: false,
+    ArrowDown: false,
+  });
+
+  useEffect(() => {
+    console.log(keyPress);
+  }, [keyPress]);
+
+  const onKeyDownHandler = (e: any) => {
+    type keyTypes = keyof typeof keyPress;
+    if (keyPress.hasOwnProperty(e.key)) {
+      const key = e.key as keyTypes;
+      setKeyPress((prev) => {
+        if (prev[key] === true) return prev;
+        const nKeyPress = { ...prev };
+        nKeyPress[key] = true;
+        return nKeyPress;
+      });
+    }
+  };
+  const onKeyUpHandler = (e: any) => {
+    type keyTypes = keyof typeof keyPress;
+    if (keyPress.hasOwnProperty(e.key)) {
+      const key = e.key as keyTypes;
+      setKeyPress((prev) => {
+        if (prev[key] === false) return prev;
+        const nKeyPress = { ...prev };
+        nKeyPress[key] = false;
+        return nKeyPress;
+      });
+    }
+  };
+
+  useFrame(({ clock }, delta) => {
+    const speed = 5;
+    console.log("delta", delta);
+    if (myMesh.current) {
+      for (let [key, value] of Object.entries(keyPress))
+        if (value === false) continue;
+        else {
+          switch (key) {
+            case "ArrowLeft": {
+              myMesh.current.position.x -= speed * delta;
+              break;
+            }
+            case "ArrowRight": {
+              myMesh.current.position.x += speed * delta;
+              break;
+            }
+            case "ArrowUp": {
+              myMesh.current.position.y += speed * delta;
+
+              break;
+            }
+            case "ArrowDown": {
+              myMesh.current.position.y -= speed * delta;
+              break;
+            }
+            default:
+              console.log("asdf?");
+          }
+        }
+    }
+  });
+
+  useFrame(({ clock }) => {
+    if (myMesh.current) {
+      const a = clock.getElapsedTime();
+      // console.log(a);
+      myMesh.current.rotation.x = a;
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDownHandler);
+    document.addEventListener("keyup", onKeyUpHandler);
+  }, []);
+  return (
+    <mesh
+      ref={myMesh}
+      visible
+      position={[1, 1, 1]}
+      rotation={[0.01, 0.01, 0.01]}
+    >
+      <meshBasicMaterial color={"black"} />
+      <boxGeometry args={[1, 1, 1]} />
+    </mesh>
+  );
+};
+
+const AirPlain = () => {
+  return (
+    <div id="world">
+      <Canvas>
+        <group>
+          <mesh
+            visible
+            userData={{ hello: "world" }}
+            position={[1, 1, 1]}
+            // rotation={[0.01, 0.01, 0.01]}
+          >
+            <meshBasicMaterial color={"black"} />
+            <boxGeometry args={[1, 1, 1]} />
+          </mesh>
+        </group>
+        <Box></Box>
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+      </Canvas>
+    </div>
+  );
+};
+export default AirPlain;
